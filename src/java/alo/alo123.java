b2c09a27fdd6fc1129bce54dc54679a4e78e8fd8
taitran <tai05112004@gmail.com>
2025-01-13 22:49:27 +0700

    newTest

---
+++ src/java/alo/alo123.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alo;
+
+/**
+ *
+ * @author OSAKA-HoaLac
+ */
+public class alo123 {
+    
+}
